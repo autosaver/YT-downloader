89ea632b9ed29f34eac57e20187f365416b203ea
ADITYA KUMAR SINGH <54054645+autosaver@users.noreply.github.com>
2022-05-30 10:20:20 +0530

    Initial commit

---
+++ contentScript.js
@@ -0,0 +1,40 @@
+(() => {
+  let youtubeRightControls;
+  let currentVideo = "";
+
+  chrome.runtime.onMessage.addListener((obj, sender, response) => {
+    const { type, value, videoId } = obj;
+
+    if (type === "NEW") {
+      currentVideo = videoId;
+      console.log("NEW", videoId);
+      newVideoLoaded(videoId);
+    }
+  });
+
+  const newVideoLoaded = () => {
+    const bookmarkBtnExists =
+      document.getElementsByClassName("bookmark-btn")[0];
+    console.log("button exists: ",bookmarkBtnExists);
+
+    if (!bookmarkBtnExists) {
+      const bookmarkBtn = document.createElement("img");
+
+      bookmarkBtn.src = chrome.runtime.getURL("assets/download.png");
+      bookmarkBtn.className ="ytp-button ytp-subtitles-button bookmark-btn"
+    bookmarkBtn.style.width = "30px";
+    bookmarkBtn.style.height = "30px";
+    bookmarkBtn.style.padding = "5px";
+
+
+      bookmarkBtn.title = "Click to Download";
+
+      youtubeRightControls =document.getElementsByClassName("ytp-right-controls")[0];
+      youtubeRightControls.prepend(bookmarkBtn);
+      bookmarkBtn.addEventListener("click", ()=>{window.open( "https://www.y2mate.com/youtube/"+currentVideo, "_blank")});
+    }
+  };
+
+  newVideoLoaded();
+})();
+
